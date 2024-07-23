d1e40fe51ef739913fab345b4cfa9732c0853ddd
icanooooo <muhihsan0@outlook.com>
2024-07-23 19:35:09 +0700

    gajadi nambahin variable di fct_reviews

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -13,6 +13,5 @@ SELECT
     {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text'])}} as review_id,
     *
 FROM src_reviews WHERE review_text IS NOT NULL
-{% if is_incremental() %}
     AND review_date > (SELECT MAX(review_date) from {{ this }})
 {% endif %}
