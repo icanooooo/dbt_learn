d99489a7fc92434084ea77426368fa95847bdf62
icanooooo <muhihsan0@outlook.com>
2024-07-23 19:27:11 +0700

    adding learn_variables macros

---
+++ macros/learn_variables.sql
@@ -0,0 +1,11 @@
+{% macro learn_variables() %}
+
+
+{% set name_jinja = "Ican" %}
+
+{{ log("Irene loves " ~ name_jinja, info=True )}}
+
+{{ log("Hello dbt user " ~ var("user_name", "Kim Yerim"), info=True)}}
+
+
+{% endmacro %}
